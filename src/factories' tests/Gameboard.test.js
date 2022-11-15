39386e482ad209be920275764528691f0798eb84
Dagmawi <dagmawinebeyu3@gmail.com>
2022-11-15 09:14:56 +0300

    Change class instance name and fix tests

--- src/factories' tests/Gameboard.test.js
+++ src/factories' tests/Gameboard.test.js
@@ -1,59 +1,57 @@
 import Gameboard from '../factories/Gameboard';
+import Ship from '../factories/Ship';
 
 describe('Gameboard', () => {
-  let gboard;
+  let gameboard;
 
   beforeEach(() => {
-    gboard = new Gameboard();
+    gameboard = new Gameboard();
   });
 
   test('Places ship at specified coord', () => {
-    gboard.placeShip([1, 3]);
+    gameboard.placeShip([1, 3], 4, 'v');
 
-    expect(gboard.board[1][3]).toEqual({
+    expect(gameboard.board[1][3]).toEqual({
       coord: [1, 3],
-      ship: {
-        hits: [],
-        length: 3,
-      },
+      axis: 'v',
+      ship: new Ship(4),
     });
   });
 
   test('Sends the ‘hit’ function to the correct ship if attack hit a ship', () => {
-    gboard.placeShip([1, 3]);
-    gboard.receiveAttack([1, 4]);
+    gameboard.placeShip([1, 3], 4, 'v');
+    gameboard.receiveAttack([2, 3]);
 
-    expect(gboard.board[1][3].ship.hits.length).toBe(1);
+    expect(gameboard.board[1][3].ship.hits.length).toBe(1);
   });
 
   test('Records the coordinates of missed shots', () => {
-    gboard.placeShip([1, 3]);
-    gboard.receiveAttack([1, 4]);
-    gboard.receiveAttack([1, 6]);
+    gameboard.placeShip([1, 3]);
+    gameboard.receiveAttack([1, 4]);
+    gameboard.receiveAttack([1, 6]);
 
-    expect(gboard.missedShots).toContainEqual([1, 6]);
+    expect(gameboard.missedShots).toContainEqual([1, 6]);
   });
 
   test('Does not hit the same target more than once', () => {
-    gboard.placeShip([1, 3]);
-    gboard.receiveAttack([1, 4]);
-    gboard.receiveAttack([1, 4]);
+    gameboard.placeShip([1, 3], 4, 'v');
+    gameboard.receiveAttack([2, 3]);
+    gameboard.receiveAttack([2, 3]);
 
-    expect(gboard.board[1][3].ship.hits.length).toBe(1);
+    expect(gameboard.board[1][3].ship.hits.length).toBe(1);
   });
 
   test('Reports whether or not all ships have been sunk', () => {
-    gboard.placeShip([1, 3]);
-    gboard.placeShip([4, 0]);
+    gameboard.placeShip([1, 3], 3, 'v');
+    gameboard.placeShip([4, 0], 2, 'h');
 
-    gboard.receiveAttack([1, 3]);
-    gboard.receiveAttack([1, 4]);
-    gboard.receiveAttack([1, 5]);
+    gameboard.receiveAttack([1, 3]);
+    gameboard.receiveAttack([2, 3]);
+    gameboard.receiveAttack([3, 3]);
 
-    gboard.receiveAttack([4, 0]);
-    gboard.receiveAttack([4, 1]);
-    gboard.receiveAttack([4, 2]);
+    gameboard.receiveAttack([4, 0]);
+    gameboard.receiveAttack([4, 1]);
 
-    expect(gboard.areAllShipsSunk()).toBeTruthy();
+    expect(gameboard.areAllShipsSunk()).toBeTruthy();
   });
 });
